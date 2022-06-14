527d671586ff15140ae8592dd6eb3f0c294ae990
ValRobichaux <valrobichaux@tamu.edu>
2022-06-14 18:02:27 -0500

    Added hamburger menu / navbar info

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Works from './components/works/Works'
 import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import "./app.scss"
-
+import { useState } from 'react'
+import Menu from './components/menu/Menu'
 
 function App() {
+  const[menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import './Navbar.scss'
 import PersonIcon from '@mui/icons-material/Person';
 import EmailIcon from '@mui/icons-material/Email';
 
-export default function navbar() {
+export default function navbar({menuOpen, setMenuOpen}) {
   return (
-    <div className='navbar'>
+    <div className={'navbar ' + (menuOpen && "active")}>
         <div className='wrapper'>
             <div className="left">
                 <a href="#intro" className='logo'>Magic.</a>
@@ -18,7 +18,13 @@ export default function navbar() {
                 </div>
             </div>
 
-            <div className="right"></div>
+            <div className="right">
+              <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                      <span className='line1'></span>
+                      <span className='line2'></span>
+                      <span className='line3'></span>
+                    </div>
+            </div>
         </div>
     </div>
   )

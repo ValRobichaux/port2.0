1f44abdbe016829409446c621b4e1f5787db8534
ValRobichaux <val.robichaux@gmail.com>
2022-06-20 21:01:26 -0500

    Added portfolio page and reactive elements

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,41 @@ import './Portfolio.scss'
 
 export default function portfolio() {
   return (
-    <div className='portfolio' id='portfolio'>portfolio</div>
+    <div className='portfolio' id='portfolio'>
+      <h1>Portfolio.</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Linux</li>
+        <li>Ios</li>
+        <li>Android</li>
+        <li>Ya mama</li>
+      </ul>
+      <div className='container'>
+        <div className='item'>
+          <img src="assets/obamium.png" alt=""></img>
+          <h3>Random App</h3>
+        </div>
+        <div className='item'>
+          <img src="assets/obamium.png" alt=""></img>
+          <h3>Random App</h3>
+        </div>
+        <div className='item'>
+          <img src="assets/obamium.png" alt=""></img>
+          <h3>Random App</h3>
+        </div>
+        <div className='item'>
+          <img src="assets/obamium.png" alt=""></img>
+          <h3>Random App</h3>
+        </div>
+        <div className='item'>
+          <img src="assets/obamium.png" alt=""></img>
+          <h3>Random App</h3>
+        </div>
+        <div className='item'>
+          <img src="assets/obamium.png" alt=""></img>
+          <h3>Random App</h3>
+        </div>
+      </div>
+    </div>
   )
 }

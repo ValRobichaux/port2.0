541bef1c9c8436955fca2accd9ba7ad55c5925e8
ValRobichaux <val.robichaux@gmail.com>
2022-06-23 22:48:44 -0500

    imported and added correct pictures for projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -13,20 +13,20 @@ export default function portfolio() {
       </ul>
       <div className='container'>
         <div className='item'>
-          <img src="assets/obamium.png" alt=""></img>
-          <h3>Random App</h3>
+          <img src="assets/Buddy_alloc.jpg" alt=""></img>
+          <h3>Buddy Allocator</h3>
         </div>
         <div className='item'>
-          <img src="assets/obamium.png" alt=""></img>
-          <h3>Random App</h3>
+          <img src="assets/clientservermodel.jpg" alt=""></img>
+          <h3>Client Server Access</h3>
         </div>
         <div className='item'>
-          <img src="assets/obamium.png" alt=""></img>
-          <h3>Random App</h3>
+          <img src="assets/linux shell.jpg" alt=""></img>
+          <h3>Custom Linux shell</h3>
         </div>
         <div className='item'>
-          <img src="assets/obamium.png" alt=""></img>
-          <h3>Random App</h3>
+          <img src="assets/TCP and IP.jpg" alt=""></img>
+          <h3>TCP and IP protocol communication</h3>
         </div>
         <div className='item'>
           <img src="assets/obamium.png" alt=""></img>

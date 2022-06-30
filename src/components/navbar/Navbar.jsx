ceaf8a3dd6e980bbcd19a6057a00d98cff228131
ValRobichaux <val.robichaux@gmail.com>
2022-06-29 23:09:12 -0500

    added linked in to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import './Navbar.scss'
 import PersonIcon from '@mui/icons-material/Person';
 import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function navbar({menuOpen, setMenuOpen}) {
   return (
@@ -16,8 +17,11 @@ export default function navbar({menuOpen, setMenuOpen}) {
                   <EmailIcon className='icon'/>
                   <span>val.robichaux@gmail.com</span>
                 </div>
+                <div className='itemContainer'>
+                  <LinkedInIcon className='icon' fontSize='large'/>
+                  <a href="https://www.linkedin.com/in/valrobichaux1129/" target="_blank" rel="noopener noreferrer">Linkedin/valrobichaux</a>
+                </div>
             </div>
-
             <div className="right">
               <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                       <span className='line1'></span>

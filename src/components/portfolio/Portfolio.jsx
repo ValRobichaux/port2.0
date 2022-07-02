1bfc42154454969da37c1fc98989558fdd2fd806
ValRobichaux <val.robichaux@gmail.com>
2022-07-01 20:18:50 -0500

    Made all portfolio items clickable / navbar fixes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,24 +5,25 @@ import {
   featuredPortfolio,
   schoolPortfolio,
   personalPortfolio,
-  otherPortfolio} from "../portfoliodata";
+  otherPortfolio
+} from "../portfoliodata";
 
 
 export default function Portfolio() {
-    const [selected,setSelected] = useState("featured");
-    const [data, setData] = useState([]);
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
 
-    
+
   const list = [
-    {id: "featured",title: "Featured"},
-    {id: "school work",title: "School Work"},
-    {id: "personal work",title: "Personal Work"},
-    {id: "other",title: "Other"},
+    { id: "featured", title: "Featured" },
+    { id: "school work", title: "School Work" },
+    { id: "personal work", title: "Personal Work" },
+    { id: "other", title: "Github" },
   ];
 
-  useEffect(()=>{
+  useEffect(() => {
 
-    switch(selected) {
+    switch (selected) {
       case "featured":
         setData(featuredPortfolio);
         break;
@@ -35,34 +36,36 @@ export default function Portfolio() {
       case "other":
         setData(otherPortfolio);
         break;
-        default:
-          setData(featuredPortfolio);
+      default:
+        setData(featuredPortfolio);
     }
 
-  },[selected])
-  
+  }, [selected])
+
   return (
     <div className='portfolio' id='portfolio'>
       <h1>Portfolio.</h1>
       <ul>
         {list.map((item) => (
           <PortfolioList
-            title={item.title} 
+            title={item.title}
             active={selected === item.id}
             setSelected={setSelected}
-            id = {item.id}
+            id={item.id}
           />
         ))}
       </ul>
       <div className='container'>
-        {data.map(d=> (
-          <div className='item'>
-          <img 
-            src={d.img} 
-            alt=""/>
-          <h3>{d.title}</h3>
-        </div>
-          ))}
+        {data.map(d => (
+          <a href={d.weblink} target="_blank" rel="noopener noreferrer">
+            <div className='item'>
+              <img
+                src={d.img}
+                alt="" />
+              <h3>{d.title}</h3>
+            </div>
+          </a>
+        ))}
       </div>
     </div>
   )
--- src/components/portfoliodata.js
+++ src/components/portfoliodata.js
@@ -3,21 +3,25 @@ export const featuredPortfolio = [
         id: 1,
         title:"Buddy Allocator",
         img: "assets/Buddy_alloc.jpg",
+        weblink:"https://github.com/ValRobichaux/CSCE-313/tree/master/PA1"
     },
     {
         id: 2,
         title:"Client Server Access",
-        img: "assets/clientservermodel.jpg"
+        img: "assets/clientservermodel.jpg",
+        weblink:"https://github.com/ValRobichaux/CSCE-313/tree/master/PA2"
     },
     {
         id: 3,
         title:"Custom Linux shell",
-        img: "assets/linux shell.jpg"
+        img: "assets/linux shell.jpg",
+        weblink:"https://github.com/ValRobichaux/CSCE-313/tree/master/PA3"
     },
     {
         id: 4,
         title:"TCP and IP communication",
-        img: "assets/TCP and IP.jpg"
+        img: "assets/TCP and IP.jpg",
+        weblink:"https://github.com/ValRobichaux/CSCE-313/tree/master/PA5"
     }
 ]
 
@@ -25,22 +29,27 @@ export const schoolPortfolio = [
     {
         id: 1,
         title:"Environment Variables",
-        img: "assets/env variable.jpg"
+        img: "assets/env variable.jpg",
+        weblink:"https://github.com/ValRobichaux/CSCE-465/tree/main/Environment%20Variables%20and%20Set%20UID"
+        
     },
     {
         id: 2,
         title:"Firewall Experimentation",
-        img: "assets/firewall.jpg"
+        img: "assets/firewall.jpg",
+        weblink:"https://github.com/ValRobichaux/CSCE-465/tree/main/Firewall%20Experimentation"
     },
     {
         id: 3,
         title:"Packet sniff/spoof",
-        img: "assets/packet sniffing and spoofing.png"
+        img: "assets/packet sniffing and spoofing.png",
+        weblink:"https://github.com/ValRobichaux/CSCE-465/tree/main/Packet%20Sniffing%20and%20Spoofing"
     },
     {
         id: 4,
         title:"SQL injection attack",
-        img: "assets/sql injection.jpg"
+        img: "assets/sql injection.jpg",
+        weblink:"https://github.com/ValRobichaux/CSCE-465/tree/main/SQL%20injection%20attack"
     }
 ]
 
@@ -48,29 +57,34 @@ export const personalPortfolio = [
     {
         id: 1,
         title:"This very website!",
-        img: "assets/portpic.jpg"
+        img: "assets/portpic.jpg",
+        weblink:"https://valrobichaux.com"
     },
     {
         id: 2,
         title:"My Dev Blog",
-        img: "assets/dev blog.jpg"
+        img: "assets/dev blog.jpg",
+        weblink:"https://github.com/ValRobichaux/devBlog"
     },
     {
         id: 3,
         title:"LeetCode Solutions",
-        img: "assets/leetcode.jpg"
+        img: "assets/leetcode.jpg",
+        weblink:"https://github.com/ValRobichaux/LEETcode-Solutions"
     },
     {
         id: 4,
         title:"SpaghettiGame",
-        img: "assets/unity.png"
+        img: "assets/unity.png",
+        weblink:"https://github.com/ValRobichaux/SpaghettiGame"
     }
 ]
 
 export const otherPortfolio = [
     {
         id: 1,
-        title:"Coming soon!",
-        img: "assets/coming soon.png"
+        title:"Github",
+        img: "assets/github.jpg",
+        weblink:"https://github.com/ValRobichaux"
     }
 ]

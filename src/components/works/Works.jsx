003da71a8e2e2ac3d9d85bf73bb2543106c522e4
ValRobichaux <val.robichaux@gmail.com>
2022-08-04 21:33:47 -0500

    Added a data set and scrolling on works page

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,29 +1,39 @@
 import "./Works.scss"
+import { workData } from "../worksdata";
+import { useState } from "react";
 
-export default function works() {
+
+export default function Works() {
+
+  const [currentSlide,setCurrentSlide] = useState(0);
+  const handleClick = (direction)=>{
+    direction === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) :
+    setCurrentSlide(currentSlide < workData.length - 1 ? currentSlide + 1 : 0)
+  }
+
   return ( <div className="works" id="works">
-      <div className="slider">
-        <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src="assets/placeholder.png" alt="" />
-                </div>
-                <h2>Title</h2>
-                <p>Description</p>
+      <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
+        {workData.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                 <div className="imgContainer">
+                  <img src={d.img} alt="" />
+                  </div>
+                <h2>{d.title}</h2>
+                <p>{d.description}</p>
                 <span>Projects</span>
               </div>
             </div>
             <div className="right">
-              <img src="assets/placeholder.png" alt=""></img>
+              <img src="assets/placeholder.png" alt=""/>
             </div>
-            
           </div>
-        </div>
-      </div>
-      <img src="assets/downarrow.png" className="arrow left" alt=""></img>
-      <img src="assets/downarrow.png" className="arrow right" alt=""></img>
+        </div>))}
+      </div> 
+      <img src="assets/downarrow.png" className="arrow left" alt="" onClick={()=>handleClick("left")}></img>
+      <img src="assets/downarrow.png" className="arrow right" alt="" onClick={()=>handleClick("right")}></img>
 
     </div>
   )
---
+++ src/components/worksdata.js
@@ -0,0 +1,20 @@
+export const workData = [
+    {
+        id: 1,
+        title: "Unity",
+        img: "assets/placeholder.png",
+        weblink:"https://unity.com"
+    },
+    {
+        id: 2,
+        title: "Machine Learning",
+        img: "assets/placeholder.png",
+        weblink: "https://google.com"
+    },
+    {
+        id: 3,
+        title: "Blogging",
+        img: "assets/placeholder.png",
+        weblink: "https://google.com"
+    }
+]

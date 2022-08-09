327e3c15db693085c7eabfe271c11ba071825289
ValRobichaux <val.robichaux@gmail.com>
2022-08-08 21:25:12 -0500

    Finished Unity description

--- src/components/worksdata.js
+++ src/components/worksdata.js
@@ -3,7 +3,7 @@ export const workData = [
         id: 1,
         icon: "assets/unityicon.png",
         title: "Unity",
-        description:"blah blah blah",
+        description:"Unity is something that excited me for a very long time, I really like the approach that the developers have used to create an extremely versatile engine that can support 2D,3D, VR and AR games.  Unity also comes along with absolutely buttery graphics and an extremely great community.  I definitely would like looking into 3D model rendering and help on some indie game projects ",
         img: "assets/unitylogo.png",
         weblink:"https://unity.com"
     },

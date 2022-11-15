4c1f87ffdeaebc5bb0a676c8455bcd41efce3d45
ValRobichaux <val.robichaux@gmail.com>
2022-11-14 21:34:17 -0600

    Finished contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,19 @@
-import "./Contact.scss"
+import "./Contact.scss";
 
 export default function contact() {
   return (
-    <div className='contact' id='contact'>contact</div>
-  )
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/placeholder.png" alt=""></img>
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }

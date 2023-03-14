9ddcad3ca8cf73601eee35e1ecb2fcdb100737b0
ValRobichaux <val.robichaux@gmail.com>
2023-03-13 21:59:21 -0500

    Refactored intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,36 +1,40 @@
-import "./Intro.scss"
-import Typewriter from 'typewriter-effect'
-
+import "./Intro.scss";
+import Typewriter from "typewriter-effect";
 
 export default function intro() {
-
   return (
     <div className="intro" id="intro">
-      <div className="left">
-        <div className="imageContainer">
-          <img src="assets/val.png" alt=""/>
+      <div className="responsive-container">
+        <div className="row">
+          <div className="left col">
+            <div className="imageContainer">
+              <img src="assets/val.png" alt="" />
+            </div>
+          
+        </div>
+        <div className="right col">
+          <div className="wrapper">
+            <h2>Hello, I'm</h2>
+            <h1>Val Robichaux</h1>
+            <h3>
+              {" "}
+              Full-time
+              <Typewriter
+                options={{
+                  strings: ["Developer", "Learner", "Tech Lover"],
+                  autoStart: true,
+                  loop: true,
+                  wrapperClassName: "typer",
+                }}
+              />
+            </h3>
           </div>
+          <a href="#portfolio">
+            <img src="assets/downarrow.png" alt=""></img>
+          </a>
         </div>
-      <div className="right">
-        <div className="wrapper">
-          <h2>Hello, I'm</h2>
-          <h1>Val Robichaux</h1>
-          <h3> Full-time
-            <Typewriter
-              options={{
-                strings:["Developer","Learner","Tech Lover"],
-                autoStart:true,
-                loop:true,
-                wrapperClassName:"typer",
-              }}
-            /> 
-          </h3>
-        </div>
-        <a href="#portfolio">
-          <img src="assets/downarrow.png" alt=""></img>
-        </a>
+      </div>
       </div>
     </div>
-      
-  )
+  );
 }

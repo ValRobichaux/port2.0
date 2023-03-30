4b07448fec10ab1cfdf38e467812e681f78f6680
ValRobichaux <val.robichaux@gmail.com>
2023-03-29 20:13:40 -0500

    intro mobile fix

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -82,22 +82,22 @@ export default function Works() {
                 <div className="row">
                   <div className="left col">
                     <div className="imagewrapper">
-                      <img src="assets/unityicon.png" alt=""></img>
+                      <img src="assets/MLicon.png" alt=""></img>
                     </div>
-                    <h1>Unity</h1>
+                    <h1>Machine Learning</h1>
                     <span className="description">
-                      Unity is something that excited me for a very long time, I
-                      really like the approach that the developers have used to
-                      create an extremely versatile engine that can support
-                      2D,3D, VR and AR games. Unity also comes along with
-                      absolutely buttery graphics and an extremely great
-                      community. I definitely would like looking into 3D model
-                      rendering and help on some indie game projects
+                      "I personally think that Machine learning is going to
+                      change the world some day. Machine learning combines
+                      statistics, data mining, and computer learning and meshes
+                      them into a unique way to observe and manipulate data.
+                      Data is only getting bigger as the world grows and being
+                      able to come up with intelligent decisions based on data
+                      will help improve quality of life for everyone. ",
                     </span>
                   </div>
                   <div className="right col">
-                    <a href="https://unity.com">
-                      <img src="assets/unitylogo.png" alt=""></img>
+                    <a href="https://www.techtarget.com/searchenterpriseai/definition/machine-learning-ML#:~:text=Machine%20learning%20(ML)%20is%20a,to%20predict%20new%20output%20values.">
+                      <img className="machinelearning" src="assets/MLmain.png" alt=""></img>
                     </a>
                   </div>
                 </div>
@@ -108,22 +108,22 @@ export default function Works() {
                 <div className="row">
                   <div className="left col">
                     <div className="imagewrapper">
-                      <img src="assets/unityicon.png" alt=""></img>
+                      <img src="assets/blogicon.png" alt=""></img>
                     </div>
-                    <h1>Unity</h1>
+                    <h1>Blogging</h1>
                     <span className="description">
-                      Unity is something that excited me for a very long time, I
-                      really like the approach that the developers have used to
-                      create an extremely versatile engine that can support
-                      2D,3D, VR and AR games. Unity also comes along with
-                      absolutely buttery graphics and an extremely great
-                      community. I definitely would like looking into 3D model
-                      rendering and help on some indie game projects
+                      "My dev blog is something that has really inspired me to
+                      build things in public and showcase the type of work that
+                      I have been doing. It's something that has really helped
+                      me embrace the reality of being a programmer and something
+                      that I love doing. I was always so interested in reading
+                      updates and patch notes for my favorite games, so this was
+                      something that I really wanted to start in my off time.",
                     </span>
                   </div>
                   <div className="right col">
-                    <a href="https://unity.com">
-                      <img src="assets/unitylogo.png" alt=""></img>
+                    <a href="https://github.com/ValRobichaux/devBlog">
+                      <img className="blog" src="assets/blogmain.png" alt=""></img>
                     </a>
                   </div>
                 </div>
@@ -132,54 +132,6 @@ export default function Works() {
           </Swiper>
         </div>
       </div>
-
-      {/* <div
-        className="slider"
-        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
-      >
-        {workData.map((d, index) => (
-          <div className="responsive-container" key={d.key}>
-            <div className="row">
-              <div
-                className="item"
-                ref={(el) => (worksItems.current[index] = el)}
-              >
-                <div className="left col">
-                  <div className="leftContainer">
-                    <div className="imgContainer">
-                      <img src={d.icon} alt="" />
-                    </div>
-                    <h2>
-                      {d.title}
-                      <p>{d.description}</p>
-                    </h2>
-                  </div>
-                </div>
-                <div className="right col">
-                  <a href={d.weblink} target="_blank" rel="noopener noreferrer">
-                    <img src={d.img} alt="" />{" "}
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      <img
-        src="assets/downarrow.png"
-        className="arrow left"
-        alt=""
-        onClick={() => handleClick("left")}
-      ></img>
-      <img
-        src="assets/downarrow.png"
-        className="arrow right"
-        alt=""
-        onClick={() => handleClick("right")}
-      ></img>
-    
-    </div> */}
     </div>
   );
 }
